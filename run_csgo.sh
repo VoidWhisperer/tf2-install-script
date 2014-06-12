1792d68b3fd67bf9135dcbef31ed360527f68f26
VoidWhisperer <voidwhisperer@voidchar.com>
2014-06-12 18:49:36 -0400

    Fix missing quote

--- run_csgo.sh
+++ run_csgo.sh
@@ -2,7 +2,7 @@ echo "Gamemode? (Casual,Competitive, Arms race, Demolition, Deathmatch)"
 read gamemode
 if [ "$gamemode" = "Casual" ]; then
 	./srcds_run -game csgo -console -usercon +game_type 0 +game_mode 0 +mapgroup mg_bomb +map de_dust
-elif [ "$gamemode" = "Competitive ]; then
+elif [ "$gamemode" = "Competitive" ]; then
 	./srcds_run -game csgo -console -usercon +game_type 0 +game_mode 1 +mapgroup mg_bomb_se +map de_dust2_se
 elif [ "$gamemode" = "Arms race" ]; then
 	./srcds_run -game csgo -console -usercon +game_type 1 +game_mode 0 +mapgroup mg_armsrace +map ar_shoots
